20a44ba960d4c8907292d49605f84e15e80ab905
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-10-11 14:41:59 -0500

    Se crea la estructura del proyecto

---
+++ src/main/java/com/temperatureReadingSpringBoot/TemperatureReadingApplication.java
@@ -0,0 +1,13 @@
+package com.temperatureReadingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureReadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temperatureReadingSpringBoot/TemperatureReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temperatureReadingSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
